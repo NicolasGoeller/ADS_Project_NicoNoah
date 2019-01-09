c558d8002e900147dc3e59777f062fc082b24d45
Nicolas <n.goeller@zeppelin-university.net>
2019-01-09 16:01:27 +0100

    Some analysis structure
    More to come

--- C_Analysis.R
+++ C_Analysis.R
@@ -13,6 +13,8 @@ library(sjPlot)
 EVS_final <- read_rds("Data/EVS_final.rds")
 
 #Subsetting data and omitting NAs
+ana_dat <- select(EVS_final, nation, reg, sat, happ, job_sat, siops, isei, sup, entre, 
+                  inc)
 
 #----------------------------------------------------------------------------------------
 
@@ -22,56 +24,46 @@ EVS_final <- read_rds("Data/EVS_final.rds")
 
 ##  1. Individual model (Level 1)
 
-m1
+m1 <- lm(sat ~ job_sat + siops + isei, data = ana_dat)
+summary(m1)
 
 #---------------------------------------------------------------------------------------------
 
 ##  2. Regional models (Level 2)
 
 #Null model for regions
-m2 <- lmer(sat ~ 1 + (1|reg), EVS_final)
+m2 <- lmer(sat ~ 1 + (1|reg), data = ana_dat)
 summary(m2)
 icc(m2)
 
 #Model with Regional and Individual predictors
-m3
+m3 <- lmer(sat ~ 1 + (1|reg), data = ana_dat)
+summary(m3)
 
 #---------------------------------------------------------------------------------------------
 
 ##  3. National models (Level 3)
 
 #Null model for nations
-m4 <- lmer(sat ~ 1 + (1|nation), EVS_final)
+m4 <- lmer(sat ~ 1 + (1|nation), data = ana_dat)
 summary(m4)
 icc(m4)
 
 #Model with National and Individual predictors
-m5
+m5 <- lmer(sat ~ 1 + (1|nation), data = ana_dat)
+summary(m5)
 
 #-----------------------------------------------------------------------------------------
 
 ##  4. Intergrated models (Level 2 & 3)
 
 #Null model for both regions and nations
-m6 <- lmer(sat ~ 1 + (1|reg) + (1|nation), EVS_final)
+m6 <- lmer(sat ~ 1 + (1|reg) + (1|nation), data = ana_dat)
 summary(m6)
 icc(m6)
 
 #Model with predictors from all levels
-m7
-
-
+m7 <- lmer(sat ~ 1 + (1|reg) + (1|nation), data = ana_dat)
+summary(m7)
 
 #---------------------------------------------------------------------------------------------------------------------------
-### Correlation analysis
-
-## 1. Life satisfaction and happiness 
-cor(EVS$sat, EVS$happ) # doesn't work 
-
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------
-### Individual-level regressions
-
-## 1. Explaining perception of income equality with own monthly income
-reg_1 <- lm(inc_eq ~ incppp_mon, data = EVS)
-summary(reg_1)
