79ee38e2f7189489d57cee1935d497d642aabe4d
Nicolas <n.goeller@zeppelin-university.net>
2019-01-13 12:32:23 +0100

    Added shiny, tryout
    European happiness observer

--- European_Happiness_Observer/server.R
+++ European_Happiness_Observer/server.R
@@ -11,21 +11,7 @@ library(shiny)
 library(shinydashboard)
 
 # Define server logic
-shinyServer(function(input, output){
+shiny::shinyServer(function(input, output){
   
 })
 
-
-# Define server logic required to draw a histogram
-#shinyServer(function(input, output) {
-   
-  #output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    #x    <- faithful[, 2] 
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   #})
-  #})
--- Z_Backup.R
+++ Z_Backup.R
@@ -62,3 +62,46 @@ soc_cap_data <- select(EVS_2008, imp_fam,
 soc_cap_data %<>% na.omit()
 cor_mat <- cor(soc_cap_data) %>% round(2) #get correlation matrix
 soc_cap_data %>% as.matrix() %>% alpha(check.keys = T) #compute Cronbach's alpha 
+
+
+###Shiny
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    #generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    #draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+})
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
+
+
+
