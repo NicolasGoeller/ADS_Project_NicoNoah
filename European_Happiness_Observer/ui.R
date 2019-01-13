9fed109837e4344333442d7bb9c1b92d3231b992
Nicolas <n.goeller@zeppelin-university.net>
2019-01-13 12:03:00 +0100

    Added Shiny, problems with the app

---
+++ European_Happiness_Observer/ui.R
@@ -0,0 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#    http://shiny.rstudio.com/
+#
+install.packages("shiny")
+install.packages("shinydashboard")
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application
+shinyUI(
+  dashboardPage(
+    dashboardHeader(),
+    dashboardSidebar(),
+    dashboardBody()
+  )
+)
+
+
+
+
+
+
+
+# Define UI for application that draws a histogram
+#shinyUI(fluidPage(
+  
+  # Application title
+  #titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  #sidebarLayout(
+   # sidebarPanel(
+    #   sliderInput("bins",
+      #             "Number of bins:",
+     #              min = 1,
+       #            max = 50,
+        #           value = 30)
+    #),
+    
+    # Show a plot of the generated distribution
+    #mainPanel(
+       #plotOutput("distPlot")
+    #)
+  #)
+#))
