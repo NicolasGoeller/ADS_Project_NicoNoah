7da6edc63a0b175928a7106e474c12ce6884ef9e
Nicolas <n.goeller@zeppelin-university.net>
2019-01-31 16:01:40 +0100

    Added user Manual

--- C_Analysis.R
+++ C_Analysis.R
@@ -6,53 +6,67 @@ library(tidyverse)
 library(lme4)
 library(sjstats)
 library(lmerTest)
-library(sjPlot)
+library(stargazer)
+
 
 #---------------------------------------------------------------------------------------
 
 #Read in dataset from B_Variable Preparation
 EVS_final <- read_rds("Data/EVS_final.rds")
 
-#Subsetting data and omitting NAs
-ana_dat <- select(EVS_final, nation, reg, sat, happ, job_sat, siops, isei, sup, entre, 
-                  inc)
-
-#----------------------------------------------------------------------------------------
-
-##Descriptive analysis
 
 #----------------------------------------------------------------------------------------
 
 ##  1. Individual model (Level 1)
 
-m1 <- lm(sat ~ job_sat + siops + isei, data = ana_dat)
+m1 <- lm(sat ~ job_sat, data = EVS_final)
 summary(m1)
+
+stargazer(m1,  #regression models 
+          type = "latex", # character vector (eg. "text" / "html" / "latex")
+          title = "Linear regression model",  # header
+          style = "default",  # style (choice see below)
+          summary = NULL,  # logical vector: output summary statistics when given data.frame
+          out = "LaTex/table1.tex", # path and output of file
+          out.header = FALSE, # logical vector: should output file contain code-header?
+          column.labels = c("Basic model"), # column labels for mod1/mod2
+          column.separate = c(1,1),  # how column labels should be assigned (label over sev. columns possible)
+          covariate.labels = c("Job satisfaction",
+                               "Constant"),
+          dep.var.caption = "Dependent Variable", # Caption (Top) of dependent variable
+          star.cutoffs = c(0.05,0.01,0.001),
+          dep.var.labels = c("Life satisfaction"))
 
 #---------------------------------------------------------------------------------------------
 
 ##  2. Regional models (Level 2)
 
-#Null model for regions
-m2 <- lmer(sat ~ 1 + (1|reg), data = ana_dat)
+m2 <- lme4::lmer(sat ~ job_sat + + (1|reg), data = ana_dat)
 summary(m2)
-icc(m2)
-
-#Model with Regional and Individual predictors
-m3 <- lmer(sat ~ 1 + (1|reg), data = ana_dat)
-summary(m3)
 
 #---------------------------------------------------------------------------------------------
 
 ##  3. National models (Level 3)
 
-#Null model for nations
-m4 <- lmer(sat ~ 1 + (1|nation), data = ana_dat)
-summary(m4)
-icc(m4)
+m3 <- lme4::lmer(sat ~ job_sat + GDPpc + (1|nation), data = EVS_final)
+summary(m3)
 
-#Model with National and Individual predictors
-m5 <- lmer(sat ~ 1 + (1|nation), data = ana_dat)
-summary(m5)
+stargazer(m3,  #regression models 
+          type = "latex", # character vector (eg. "text" / "html" / "latex")
+          title = "Multilevel linear regression model",  # header
+          style = "default",  # style (choice see below)
+          summary = NULL,  # logical vector: output summary statistics when given data.frame
+          out = "LaTex/table2.tex", # path and output of file
+          out.header = FALSE, # logical vector: should output file contain code-header?
+          column.labels = c("Multilevel model"), # column labels for mod1/mod2
+          column.separate = c(1,1),  # how column labels should be assigned (label over sev. columns possible)
+          covariate.labels = c("Job satisfaction",
+                               "GDP per capita",
+                               "Constant"),
+          dep.var.caption = "Dependent Variable", # Caption (Top) of dependent variable
+          star.cutoffs = c(0.05,0.01,0.001),
+          dep.var.labels = c("Life satisfaction"))
+
 
 #-----------------------------------------------------------------------------------------
 
